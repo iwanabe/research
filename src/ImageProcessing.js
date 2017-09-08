ffe010524b509a2b3d2a29eb79f686016bcd83d6
iwanabe <touhu1009@gmail.com>
2017-09-08 18:13:41 +0900

    Add files via upload

--- src/ImageProcessing.js
+++ src/ImageProcessing.js
@@ -61,35 +61,35 @@ Laplacian = function(srcImg, dstImg) {
 			if (i == 0 || i == (height-1) 
 			|| j == 0 || j == (width-1)) 
 			{
-		dst[idx] = src[idx];
-		dst[idx+1] = src[idx+1];
-		dst[idx+2] = src[idx+2];
-		dst[idx+3] = 255;
-		}
-		else {
-			var idx0 = (j-1 + (i-1)*width)*4;
-			var idx1 = (j + (i-1)*width)*4;
-			var idx2 = (j+1 + (i-1)*width)*4;
-			var idx3 = (j-1 + i*width)*4;
-			var idx4 = (j + i*width)*4;
-			var idx5 = (j+1 + i*width)*4;
-			var idx6 = (j-1 + (i+1)*width)*4;
-			var idx7 = (j + (i+1)*width)*4;
-			var idx8 = (j+1 + (i+1)*width)*4;
+				dst[idx] = src[idx];
+				dst[idx+1] = src[idx+1];
+				dst[idx+2] = src[idx+2];
+				dst[idx+3] = 255;
+			}
+			else {
+				var idx0 = (j-1 + (i-1)*width)*4;
+				var idx1 = (j + (i-1)*width)*4;
+				var idx2 = (j+1 + (i-1)*width)*4;
+				var idx3 = (j-1 + i*width)*4;
+				var idx4 = (j + i*width)*4;
+				var idx5 = (j+1 + i*width)*4;
+				var idx6 = (j-1 + (i+1)*width)*4;
+				var idx7 = (j + (i+1)*width)*4;
+				var idx8 = (j+1 + (i+1)*width)*4;
 			
-			dst[idx] = w[0]*src[idx0] + w[1]*src[idx1] + w[2]*src[idx2] +
-				w[3]*src[idx3] + w[4]*src[idx4] + w[5]*src[idx5] + 
-				w[6]*src[idx6] + w[7]*src[idx7] + w[8]*src[idx8];
-			
-			dst[idx+1] = w[0]*src[idx0+1]+w[1]*src[idx1+1]+w[2]*src[idx2+1] +
-				w[3]*src[idx3+1]+w[4]*src[idx4+1]+w[5]*src[idx5+1] + 
-				w[6]*src[idx6+1]+w[7]*src[idx7+1]+w[8]*src[idx8+1];
-			
-			dst[idx+2] = w[0]*src[idx0+2]+w[1]*src[idx1+2]+w[2]*src[idx2+2] +
-				w[3]*src[idx3+2]+w[4]*src[idx4+2]+w[5]*src[idx5+2] + 
-				w[6]*src[idx6+2]+w[7]*src[idx7+2]+w[8]*src[idx8+2];
-			
-			dst[idx+3] = 255;
+				dst[idx] = w[0]*src[idx0] + w[1]*src[idx1] + w[2]*src[idx2] +
+					w[3]*src[idx3] + w[4]*src[idx4] + w[5]*src[idx5] + 
+					w[6]*src[idx6] + w[7]*src[idx7] + w[8]*src[idx8];
+					
+				dst[idx+1] = w[0]*src[idx0+1]+w[1]*src[idx1+1]+w[2]*src[idx2+1] +
+					w[3]*src[idx3+1]+w[4]*src[idx4+1]+w[5]*src[idx5+1] + 
+					w[6]*src[idx6+1]+w[7]*src[idx7+1]+w[8]*src[idx8+1];
+					
+				dst[idx+2] = w[0]*src[idx0+2]+w[1]*src[idx1+2]+w[2]*src[idx2+2] +
+					w[3]*src[idx3+2]+w[4]*src[idx4+2]+w[5]*src[idx5+2] + 
+					w[6]*src[idx6+2]+w[7]*src[idx7+2]+w[8]*src[idx8+2];
+				
+				dst[idx+3] = 255;
 			}
 		}
 	}
