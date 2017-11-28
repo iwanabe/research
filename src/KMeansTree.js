bbb2ce9940edf0caddd896c25a54bdda5cb84c42
RikiIwanabe <touhu1009@gmail.com>
2017-11-28 14:39:09 +0900

    kmeans update

--- src/KMeansTree.js
+++ src/KMeansTree.js
@@ -11,7 +11,7 @@ KMeansTree = function(w, h, cntList){
 	divison[3] = [];
 
 	InitG(center, w, h);
-	console.log("ssss"+cntList.length);	
+	console.log("def");
 	console.log(center[0].x +"," + center[0].y);
 	console.log(center[1].x +"," + center[1].y);
 	console.log(center[2].x +"," + center[2].y);
@@ -19,9 +19,8 @@ KMeansTree = function(w, h, cntList){
 
 	
 	StoreG(center, cntList, divison,w,h);
-	
 	//test number of loop
-	for(var n = 0; n < 3 ; n=(n+1)|0){
+	for(var n = 0; n < 8 ; n=(n+1)|0){
 		CalcG(center, divison);
 		//_divison delete;
 		divison[0] = [];
@@ -73,7 +72,7 @@ CalcG = function(_center, _divison){
 }
 
 StoreG = function(_center, cntList,_divison,w,h){
-	var d = Number.MAX_VALUE;
+	
 	var dt;
 	var c;
 	var count0=0;
@@ -82,19 +81,20 @@ StoreG = function(_center, cntList,_divison,w,h){
 	var count3=0;
 	
 	for(var i = 0; i < cntList.length; i=(i+1)|0){
+		var d = Number.MAX_VALUE;
 		var p = cntList[i];
-
+		
 		for(var j = 0; j < 4; j=(j+1)|0){
 			var ppp = _center[j];
 		//	console.log(ppp.x+","+ppp.y+" "+p.x+","+p.y);
 			dt = (ppp.x - p.x)*(ppp.x - p.x) + (ppp.y - p.y)*(ppp.y - p.y);
 			
-			if(dt < d){
+			if(dt <= d){
 				d = dt;
 				c = j;
 			}
 		}
-		console.log(c);
+	//	console.log(c);
 		_divison[c].push(new Point2d(p.x, p.y));
 		if(c==0){count0++;}
 		if(c==1){count1++;}
